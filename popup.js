7fd0a21055b53467d6e6e9955b52a2a3cb668c02
Rakeshdevcoder <rakesh123dmx@gmail.com>
2025-03-16 14:46:18 +0530

    created referesh button and check if user is on linkedin.com if yes then run fetchlinkedin messages function else give error

--- popup.js
+++ popup.js
@@ -6,4 +6,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let allMessages = [];
   let currentFilter = "all";
+
+  // Load messages when popup opens
+  checkLinkedInAndLoadMessages();
+
+  refreshButton.addEventListener("click", function () {
+    fetchMessagesFromLinkedIn();
+  });
+
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      filterButtons.forEach((btn) => btn.classList.remove("active"));
+      this.classList.add("active");
+      currentFilter = this.getAttribute("data-filter");
+      displayMessages(allMessages);
+    });
+  });
+
+  // Check if user opened  LinkedIn.com  else tell user to go to linked.com website
+  function checkLinkedInAndLoadMessages() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const currentTab = tabs[0];
+      if (currentTab && currentTab.url.includes("linkedin.com")) {
+        fetchMessagesFromLinkedIn();
+      } else {
+        showStatus("Please go to LinkedIn website", "error");
+      }
+    });
+  }
 });
